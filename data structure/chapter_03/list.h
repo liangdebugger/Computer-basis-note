c7c733d3dfc1c4011c0c1b4078ed393c5b614fd4
LiangChao <liangchaox@foxmail.com>
2021-10-29 15:31:36 +0800

    Update list.h

--- data structure/chapter_03/list.h
+++ data structure/chapter_03/list.h
@@ -6,50 +6,38 @@
 class Node
 {
 public:
-	Node() :data(0), deleted(false), next(NULL) {}
-	Node(int item) :data(item), deleted(false), next(nullptr) {}
-	//Not to copy single node. Let the class 'LinkedList' do the job.
-	Node(const Node&) = delete;
-	Node& operator=(const Node&) = delete;
-
-public:
 	int data;
-	bool deleted;
 	Node* next;
 };
 
-class LinkedList {
-	friend std::istream& operator>>(std::istream&, LinkedList&);
-	friend std::ostream& operator<<(std::ostream&, const LinkedList&);
+class LinkList 
+{
+	friend std::istream& operator>>(std::istream&, LinkList&);
+	friend std::ostream& operator<<(std::ostream&, const LinkList&);
 public:
-	LinkedList() : count(0), de_count(0), no_de_count(0) {}
-	LinkedList(const LinkedList&);    
-	LinkedList& operator=(LinkedList);
-	~LinkedList();
+	LinkList();																	
+	LinkList(const LinkList&) = delete;    
+	LinkList& operator=(LinkList) = delete;
+	~LinkList();
 
-	bool IsEmpty() const { return head->next == nullptr; }
-	bool IsLast(Node* p) const { return p->next == nullptr; }
-	size_t Size() const { return count; }
-	Node* Header() { return head; }
-	Node* First() const { return head->next; }
-	Node* Advance(const Node* p) const { return p->next; }  
-	int Restrieve(Node* p) const { return p->data; }
+	bool IsEmpty() const { return head->next == nullptr; }						//判断是否为空链表
+	bool IsLast(Node* p) const { return p->next == nullptr; }					//判断是否是最后一个元素
+	
+	size_t Size() const;														//返回元素数量
+	Node* Header() { return head; }												//返回表头
+	Node* First() const { return head->next; }									//返回第一个元素
+	void MakeEmpty();
 
-	void Reverse();
-	void RemoveDup();
-	Node* Find(int);   //return nullptr if not found
-	Node* Find(int, Node*);
-	Node* FindPrevious(int); //return the last element if not found
-	void Insert(int, Node*); //insert an element after designated Node
-	void LazyDelete(int);
-	void Delete(int);
-	void DeleteList();
-	LinkedList& MakeEmpty();
+	void Insert(int d,int n=-1);												//默认在尾部插入元素
+	void Insert(int d, Node* p);
+	void Delete(int n = -1);
+
+	Node* Find(int);   															//查找
+	Node* FindPrevious(int);													//返回前一个元素
+	
 
 private:
-	Node* head = new Node;   //note that it's a dummy node(also called header) !!!
-	size_t count;  //the number of Nodes
-	size_t de_count;   //the number of being deleted for lazy deletion
-	size_t no_de_count;  //the number of not being deleted for lazy deletion
+	Node* head = new Node;
 };
+
 #endif
